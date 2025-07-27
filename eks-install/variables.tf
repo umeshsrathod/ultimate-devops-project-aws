c6fbfa03facb747e416f66a91e2b4ee999a6f7bf
Umesh Rathod <122077247+umeshsrathod@users.noreply.github.com>
2025-07-27 14:51:11 +0530

    Update variables.tf

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "ap-south-1"
 }
 
 variable "vpc_cidr" {
@@ -13,7 +13,7 @@ variable "vpc_cidr" {
 variable "availability_zones" {
   description = "Availability zones"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["ap-south-1a", "ap-south-1b"]
 }
 
 variable "private_subnet_cidrs" {
