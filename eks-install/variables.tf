bc0731c3b0592badd4db5e6b7013cd7771d36ce6
Umesh Rathod <122077247+umeshsrathod@users.noreply.github.com>
2025-07-27 15:11:05 +0530

    Update variables.tf

--- eks-install/variables.tf
+++ eks-install/variables.tf
@@ -19,7 +19,7 @@ variable "availability_zones" {
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "public_subnet_cidrs" {
