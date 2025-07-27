04bd12b4de9d84dc4c477ab97837b3547395b569
Umesh Rathod <122077247+umeshsrathod@users.noreply.github.com>
2025-07-27 14:18:51 +0530

    Update main.tf

--- eks-install/main.tf
+++ eks-install/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
+    bucket         = "demo-terraform-eks-state-s3-bucket121"  #always take unique name
     key            = "terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-eks-state-locks"
